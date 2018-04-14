e25d80a9eea65560c8dae0eb7e3c0c635c033a1b
Istyatur <alexanderslanski@sbcglobal.net>
2018-04-14 15:33:29 -0400

    added embedder scaffolding

---
+++ embbeder.py
@@ -0,0 +1,11 @@
+
+#add whatever else we need
+def getWordEmbedding(word):
+	
+
+#sentence is a unicode string.
+def makeSentenceEmbeddings(sentence):
+	doc = spacy.nlp(sentence)
+	#DO STUFFS
+	getWordEmbedding(word)
+	return embedding
